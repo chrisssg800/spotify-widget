9f4a9a371a497e25e3bde06e5f41cdfcc518c0d5
chrisssg800 <chrisssg800@gmail.com>
2026-08-19 00:31:07 +0300

    Update script.js

--- configure/script.js
+++ configure/script.js
@@ -7,7 +7,7 @@ const urlParams = new URLSearchParams(queryString);
 
 const code = urlParams.get("code") || "";
 
-const baseURL = "https://nuttylmao.github.io/spotify-widget";
+const baseURL = "https://github.com/chrisssg800/spotify";
 const redirect_uri = `${baseURL}/configure`;
 let refresh_token = "";
 let access_token = "";
